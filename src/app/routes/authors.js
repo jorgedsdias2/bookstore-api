e5491ebb4099112507511c780559f2f0ae3cdbf4
Jorge Dias <jorgedsdias@gmail.com>
2019-03-05 13:56:44 -0300

    Fazendo um teste com a rota de autores

--- src/app/routes/authors.js
+++ src/app/routes/authors.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const Author = require(__root + 'src/app/db/author');
 const verifyToken = require(__root + 'src/app/auth/verify-token');
 
+let status = 0;
+let message = '';
+
 router.post('/author', verifyToken, function(req, res) {
     req.assert('name', 'Name can not be empty').notEmpty();
 
@@ -12,26 +15,37 @@ router.post('/author', verifyToken, function(req, res) {
 
     Author.create(req.body)
     .then(author => {
-        console.log('Author created - ID: ' + author.id);
-        res.status(200).send(author);
+        message = 'Author created - ID: ' + author.id;
+        console.log(message);
+        res.status(200).send({message: message, author: author});
     })
     .catch(err => {
-        console.log('Author not created - Error: ' + err);
-        res.status(500).send(err);
+        message = 'Author not created.';
+        console.log(message);
+        res.status(500).send({message: message, error: err});
     });
 });
 
 router.get('/author/:id', verifyToken, function(req, res) {
     const id = req.params.id;
-
+    
     Author.findById(id)
     .then(author => {
-        console.log('Author found - ID: ' + author.id)
-        res.status(200).send(author);
+        if(!author) {
+            message = 'Author not found - ID: ' + author.id;
+            status = 404;
+        } else {
+            message = 'Author found - ID: ' + author.id;
+            status = 200;
+        }
+        
+        console.log(message);
+        res.status(status).send({message: message, author: author});
     })
     .catch(err => {
-        console.log('Author not found - Error: ' + err)
-        res.status(500).send(err);
+        message = 'Author error.';
+        console.log(message);
+        res.status(500).send({message: message, error: err});
     });
 });
 
