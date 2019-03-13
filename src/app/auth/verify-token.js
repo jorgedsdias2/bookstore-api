a4b1b90aad27c15261bbbf2cb20a30fc39c4515a
Jorge Dias <jorgedsdias@gmail.com>
2019-03-13 12:57:59 -0300

    Correcao secret

--- src/app/auth/verify-token.js
+++ src/app/auth/verify-token.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-const env = require(__root + 'src/config/environment');
+const environment = require(__root + 'src/config/environment');
 
 function verifyToken(req, res, next) {
 
     const token = req.headers['x-access-token'];
     if(!token) return res.status(403).send({auth: false, message: 'No token provided.'});
 
-    jwt.verify(token, env.secret, function(err, decoded) {
+    jwt.verify(token, __secret, function(err, decoded) {
         if(err) return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
         
         req.userId = decoded.id;
